05f3f5302111f636d149f839a1b81845f133fb72
Nancy Mdlangaso <nmdlanagso@c5r10s10.wethinkcode.co.za>
2020-07-15 11:23:06 +0200

    a

---
+++ ex00/main.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmdlanags <marvin@42.fr>                   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/15 09:01:04 by nmdlanags         #+#    #+#             */
+/*   Updated: 2020/07/15 11:21:20 by nmdlanags        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+
+void rush00(int x, int y);
+void rush01(int x, int y);
+void rush02(int x, int y);
+void rush03(int x, int y);
+void rush04(int x, int y);
+
+
+int     main(void)
+{
+
+	rush(5,5);
+	return(0);
+
+}
