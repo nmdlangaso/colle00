784a21e8f97903d45a93b89e684bc4f6b18696c9
Nancy Mdlangaso <nmdlanagso@c5r10s10.wethinkcode.co.za>
2020-07-15 11:57:07 +0200

    a

---
+++ ex00/rush.c
@@ -0,0 +1,18 @@
+#include<unitd.h>
+#include<stdio.h>
+int main()
+{
+	printf("%d",rush[(5,3)] );
+write( "o---o\n||\n o---o\n $>",16  );
+
+	printf("%d\n",rush[(5,1)]);
+	writ("o---o\n$>",8);
+
+	print ("%d\n",rush([1,1]);
+	write("o",1);
+
+	print("%d\n", rush[(1,5)])
+	write("o\n |\n|\n|\n o",9);
+	rush (5, 5);
+	return(0);
+}
